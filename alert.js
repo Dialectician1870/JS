60de317c4ce02877d4843de94fb0b5927c156e46
Shakal <pachan222@mail.ru>
2024-05-26 12:13:18 +0300

    12_lesson

--- alert.js
+++ alert.js
@@ -1,1 +1,17 @@
-console.log(1);
+const numberOfFilms = +prompt('Cколько фильмов смотрели?');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('ласт фильм?');
+const b = prompt('Оценка?');
+const c = prompt('ласт фильм?');
+const d = prompt('Оценка?');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
