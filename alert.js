aa00d0db00203d710c8ee484b4c3a0b601d7daa3
Shakal <pachan222@mail.ru>
2024-05-27 14:54:28 +0300

    15 lesson

--- alert.js
+++ alert.js
@@ -1,4 +1,33 @@
+/* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+'use strict';
+
+let flag = 0;
 const numberOfFilms = +prompt('Cколько фильмов смотрели?');
+if (numberOfFilms < 10) {
+    alert('Просмотрено довольно мало фильмов');
+
+} else if (numberOfFilms <= 30 && numberOfFilms >= 10) {
+    alert('Вы классический зритель');
+
+} else if (numberOfFilms > 30) {
+    alert('Вы киноман');
+
+} else {
+    alert('Произошла ошибка')
+}
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -8,10 +37,26 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('ласт фильм?');
-const b = prompt('Оценка?');
-const c = prompt('ласт фильм?');
-const d = prompt('Оценка?');
+while (flag < 2) {
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    const a = prompt('ласт фильм?');
+
+    if (a.length < 50 && a != null && a != '') {
+
+        while (1) {
+
+            const b = prompt('оценка??');
+
+            if (b != null && b != '') {
+
+                personalMovieDB.movies[a] = b;
+                flag++;
+                break;
+
+            } 
+
+        }
+
+    }
+
+}
