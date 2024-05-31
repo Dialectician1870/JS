2210ce67daa9a911412e350f2c776b145a969a81
Shakal <pachan222@mail.ru>
2024-05-31 12:24:26 +0300

    32 lesson, parent-elements

--- script.js
+++ script.js
@@ -1,56 +1,36 @@
-const btn = document.querySelectorAll('button'),
-    overlay = document.querySelector('.overlay');
+// console.log(document.head);
+//выводит только бошку
+// console.log(document.documentElement);
+//выводит фулл структуру html
 
-// btn.onclick = function() {
-//     alert('click');
-// };
-// btn.onclick = function() {
-//     alert('2nd click');
-// };
-//не юзабельное
+// console.log(document.body.childNodes);
+//позволяет получить все элементы внутри бади, для него аналога с Element
 
-// btn.addEventListener('click', () => {
-//     alert('click');
-// });
-// btn.addEventListener('click', () => {
-//     alert('2nd click');
-// });
-//здесь первое действие будет работать со вторым,
+// console.log(document.body.firstChild);
+// console.log(document.body.lastChild);
+//угадай что
+// console.log(document.body.firstElementChild);
+//чекай коммент на 20 строчке
 
-// let i = 0;
-const deleteElement = (e) => {
-    // console.log(e.target);
-    //сначала работает на вложенном элементе, а потом вверх по дереву
-    //НАЗЫВАЕТСЯ  ВСПЛЫТИЕМ СОБЫТИЙ!
-    console.log(e.currentTarget);
-    //разница в том, что мы ссылаемся на разные элементы, после, если можно так сказать, всплытия
-    console.log(e.type);
-    // i++;    
-    // if(i == 1) {
-    //     btn.removeEventListener('click', deleteElement);
-    // }
-};
+// console.log(document.querySelector('#current').parentNode);
+//вернет родителя current
+// console.log(document.querySelector('#current').parentNode.parentNode);
+//вернет родителя родителя current
+// console.log(document.querySelector('#current').parentElement);
+//Особо рофла не понял, по сути то же самое, только сто проц вернется элемент
 
-// btn.addEventListener('mouseenter', (e) => {
-//     /*e = event(первый, обязательный аргумент), 
-//     используется, если надо указать чето в качестве второго аргумента*/
-//     console.log('hover');
-//     console.log(e.target);
-//     e.target.remove();
-// });
+// console.log(document.querySelector('[data-current="3"]').nextSibling);
+//вернет следущего соседа
+// console.log(document.querySelector('[data-current="3"]').previousSibling);
+//вернет предыдущего соседа
+// console.log(document.querySelector('[data-current="3"]').previousElementSibling);
+//тоже самое, только вернет элемент нормальный а не ебучий текст(ноду), который обозначает перенос строки
 
-// btn.addEventListener('click', deleteElement);
-// overlay.addEventListener('click', deleteElement);
+for (let node of document.body.childNodes) {
+    if (node.nodeName == '#text' || node.nodeName == '#comment') {
+        continue;
+    }
 
-const link = document.querySelector("a");
-
-link.addEventListener('click', function(e) {
-    e.preventDefault();
-    //"браузер, стой, ждем че скажет прогер", отменили стандарты поведения браузера, юзаем в начале блока-кода
-    console.log(e.target);
-})
-
-btn.forEach(bt => {
-    bt.addEventListener('click', deleteElement, {once: true});
-    //с помощью этой опции, мы можем юзать эту хуйню, только 1 раз
-});
+    console.log(node);
+}
+//замена childNodes, чтобы выводила только элементы
